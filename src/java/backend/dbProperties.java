60b751bfe3559029a4382eeb1d29290c259a9722
nelsonkimaiga <nelsonkimaiga@aol.com>
2017-06-29 08:46:02 +0300

    setup backend

---
+++ src/java/backend/dbProperties.java
@@ -0,0 +1,32 @@
+package backend;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.ResourceBundle;
+
+public class dbProperties {
+	public static Connection getDBConnection() throws Exception{
+		Connection conn = null;
+		Class.forName("com.mysql.jdbc.Driver").newInstance();
+		conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/briclyn","root","303seminarian");
+      return conn;
+	}
+	
+	public static Connection getConnection() throws Exception{
+		Connection conn = null;
+		
+		ResourceBundle rb=ResourceBundle.getBundle("connection_config");
+		
+		String sDriverName=rb.getString("driver.name");
+		String sServerName=rb.getString("server.name");
+		String sPort=rb.getString("port.no");
+		String sDatabaseName=rb.getString("database.name");
+		String sUserName=rb.getString("user.name");
+		String sPassword=rb.getString("password");
+		
+		
+		Class.forName(sDriverName).newInstance();
+		conn = DriverManager.getConnection("jdbc:mysql://"+sServerName+":"+sPort+"/"+sDatabaseName,sUserName, sPassword);
+      return conn;
+	}
+}
